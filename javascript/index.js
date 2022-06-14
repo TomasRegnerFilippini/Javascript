1f64ef9cc0f7f8fa8f511aa44b91668dbf961ffb
tomas regner <tomasregnerfilippini@gmail.com>
2022-06-14 12:56:01 -0300

    actividad arrays

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,26 @@
+let total = 0;
 class producto {
     constructor(nombre, precio){
         this.nombre = nombre.toUpperCase();
         this.precio = parseFloat(precio);
         this.vendido = false;
     }
-    sumaIva(){
+    iva(){
         this.precio = this.precio * 1.21;
     }
 
-    vender(){
-        this.vendido = true;
-    }
 }
 
-const producto1 = new producto("Buzo Attack on Titan , 2500 ")
-const producto2 = new producto("Buzo Naruto , 2400 ")
-const producto3 = new producto("Buzo tokyo revengers , 2500 ")
+const productos =[];
+productos.push(new producto("Buzo Attack on Titan ", 2500));
+productos.push(new producto("Buzo Naruto ", 2400 ));
+productos.push(new producto("Buzo tokyo revengers" , 2500 ));
+
+for (const producto of productos){
+      producto.precio;
+      producto.iva
+      total += producto.precio;
+      console.log(total)
+    }
+
 
