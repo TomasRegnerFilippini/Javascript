e1531cd7a86204515088ebb8103c45daee0ca378
tomas regner <tomasregnerfilippini@gmail.com>
2022-07-06 23:00:24 -0300

    librerias

--- javascript/main.js
+++ javascript/main.js
@@ -9,41 +9,44 @@ if(JSON.parse(localStorage.getItem("carrito"))){
     carrito = JSON.parse(localStorage.getItem("carrito"))
   }
 
-
+  
+  
   function showProducts() {
-
+    
     for (let i = 0; i < products.length; i++) {
-        const element = products[i];
-        const {id, category , name ,price , img} = element
-        const card = `
-            <div class="card">
-                    <p>${name}</p>
+      const element = products[i];
+      const {id, category , name ,price , img} = element
+      const card = `
+      <div class="card">
+      <p>${name}</p>
                 <div>
-                    <img class="imgProducto" src=${img} alt="/>
+                <img class="imgProducto" src=${img} alt="/>
                 </div>
                 <div>
-                    <p class="price">$${price}</P>
+                <p class="price">$${price}</P>
                 </div>
                 <div class "addBoton">
-                    <button id=${id} class="btnAgregar"> AGREGAR AL CARRITO </BUTTON>
+                <button id=${id} class="btnAgregar"> AGREGAR AL CARRITO </BUTTON>
                 </div>
             </div>   
        `
 
        const container = document.getElementById("container")
        container.innerHTML += card
+      }
+      
     }
     
-  }
+    const btnAgregar = document.getElementsByClassName("btnAgregar")
 
-  const btnAgregar = document.getElementsByClassName("btnAgregar")
+   
 
-  for (let i = 0; i < btnAgregar.length; i++) {
-    const element = btnAgregar [i];
-    element.addEventListener("clik" , agregarAlCarrito)
+    for (let i = 0; i < btnAgregar.length; i++) {
+      const element = btnAgregar [i];
+      element.addEventListener("clik" , agregarAlCarrito)
+      
+    }
     
-  }
-
   showProducts()
 
   function agregarAlCarrito(e) {
@@ -52,6 +55,14 @@ if(JSON.parse(localStorage.getItem("carrito"))){
     const prodEncontrado = products.find(prod = prod.id == idBoton)
     const enCarrito = carrito.find(prod => prod.id == prodEncontrado.id)
     console.log(enCarrito)
+    Swal.fire({
+      position: 'top',
+      icon: 'success',
+      title: 'agregaste'+ products.name +'al carrito',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    
     if(!enCarrito){
          carrito.push({...prodEncontrado, cantidad : 1})
     } else {
